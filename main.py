e87899344bb30e4cc0e96544dd37212946160834
GoshaLetov <KhvoshchevKMwork@yandex.ru>
2021-12-14 10:08:07 +0300

    Add 2 plots

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
+import requests
+
 import pandas as pd
 import streamlit as st
-from plotly.graph_objects import Figure, Scatter, Bar
+from streamlit_lottie import st_lottie
+from plotly.graph_objects import Figure, Scatter
 
 st.set_page_config(layout="wide")
+
+DATA_FED_BUDGET_URL = 'https://minfin.gov.ru'
+DATA_MOSCOW_GDP_URL = 'https://budget.mos.ru/budget'
+IMAGE_URL_1 = 'https://freelance.ru/img/portfolio/pics/00/2F/99/3119447.jpg'
+LOTTIE_URL_1 = 'https://assets6.lottiefiles.com/private_files/lf30_rysgr4xj.json'
 
 
 @st.cache(persist=True)
@@ -14,8 +22,40 @@ def load_data():
     return table
 
 
+row1_1, row1_2 = st.columns((4, 2))
+
+with row1_1:
+    st.title(
+        """
+        Ключевые показатели исполнения Федерального Бюджета РФ за период с 2006 по 2020 годы
+        """)
+    st.subheader(
+        f"""
+        Источник данных:
+        [Официальный сайт Министерства Финансов РФ]({DATA_FED_BUDGET_URL})
+
+        [Портал Правительства Москвы "Открытый бюджет"]({DATA_MOSCOW_GDP_URL})
+        """)
+    st.subheader(
+        """
+        Выполнили:
+        Студенты 407 группы
+
+        Хвощев Кирилл и Черхаров Роман
+        """)
+
+
+image = requests.get(IMAGE_URL_1)
+with row1_2:
+    st.image(image.content, use_column_width=True)
+
+st.subheader(' ')
+st.subheader('Сравнение ключевых показателей бюджета Москвы и Федерального бюджета')
+
 data = load_data()
 
+
+st.title('Сравнение доходов, млрд. рублей')
 row1_1, row1_2 = st.columns((3, 1))
 with row1_2:
     st.subheader(' ')
@@ -37,7 +77,7 @@ with row1_1:
                   (data['Вид'] == 'Доходы') &
                   (data['Регион'] == 'Москва') &
                   (data['Наименование'] == option)
-              ].iloc[:, 2:].values[0],
+              ].iloc[:, 3:].values[0],
             stackgroup='one',
         ))
         fig.add_trace(Scatter(
@@ -47,13 +87,33 @@ with row1_1:
                   (data['Вид'] == 'Доходы') &
                   (data['Регион'] == 'Россия') &
                   (data['Наименование'] == option)
-              ].iloc[:, 2:].values[0],
+              ].iloc[:, 3:].values[0],
             stackgroup='one',
         ))
         fig.update_layout(legend=dict(orientation="h", yanchor="bottom", y=1.02, xanchor="right", x=1))
         fig.update_xaxes(tickvals=[2017, 2018, 2019, 2020])
     st.plotly_chart(fig, use_container_width=True)
 
+st.title('Сравнение доходов, %')
+fig = Figure()
+fig.add_trace(Scatter(
+    x=[2017, 2018, 2019, 2020],
+    y=((data[
+           (data['Вид'] == 'Доходы') & (data['Регион'] == 'Россия') & (data['Наименование'] == option)
+       ].iloc[:, 3:].values[0]) /
+       data[
+           (data['Вид'] == 'Доходы') & (data['Регион'] == 'Москва') & (data['Наименование'] == option)
+       ].iloc[:, 3:].values[0])
+))
+fig.update_layout(legend=dict(orientation="h", yanchor="bottom", y=1.02, xanchor="right", x=1))
+fig.update_xaxes(tickvals=[2017, 2018, 2019, 2020])
+st.plotly_chart(fig, use_container_width=True)
+
+lottie = requests.get(LOTTIE_URL_1)
+st_lottie(lottie.json(), speed=1, height=200, key='initial')
+
+
+st.title('Сравнение расходов, млрд. рублей')
 row2_1, row2_2 = st.columns((3, 1))
 with row2_2:
     st.subheader(' ')
@@ -75,7 +135,7 @@ with row2_1:
                   (data['Вид'] == 'Расходы') &
                   (data['Регион'] == 'Москва') &
                   (data['Наименование'] == option)
-              ].iloc[:, 2:].values[0],
+              ].iloc[:, 3:].values[0],
             stackgroup='one',
         ))
         fig.add_trace(Scatter(
@@ -85,9 +145,25 @@ with row2_1:
                   (data['Вид'] == 'Расходы') &
                   (data['Регион'] == 'Россия') &
                   (data['Наименование'] == option)
-              ].iloc[:, 2:].values[0],
+              ].iloc[:, 3:].values[0],
             stackgroup='one',
         ))
         fig.update_layout(legend=dict(orientation="h", yanchor="bottom", y=1.02, xanchor="right", x=1))
         fig.update_xaxes(tickvals=[2017, 2018, 2019, 2020])
     st.plotly_chart(fig, use_container_width=True)
+
+
+st.title('Сравнение расходов, %')
+fig = Figure()
+fig.add_trace(Scatter(
+    x=[2017, 2018, 2019, 2020],
+    y=((data[
+           (data['Вид'] == 'Расходы') & (data['Регион'] == 'Россия') & (data['Наименование'] == option)
+       ].iloc[:, 3:].values[0]) /
+       data[
+           (data['Вид'] == 'Расходы') & (data['Регион'] == 'Москва') & (data['Наименование'] == option)
+       ].iloc[:, 3:].values[0])
+))
+fig.update_layout(legend=dict(orientation="h", yanchor="bottom", y=1.02, xanchor="right", x=1))
+fig.update_xaxes(tickvals=[2017, 2018, 2019, 2020])
+st.plotly_chart(fig, use_container_width=True)
